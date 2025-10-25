2110dd448a9b4f41a1f03cee8118c20d8db54d6f
ak0kombat <ak0kombat@gmail.com>
2025-10-25 11:55:12 +0530

    Update OurFather.jsx

--- src/routingComponents/OurFather.jsx
+++ src/routingComponents/OurFather.jsx
@@ -1,32 +1,22 @@
 import React from "react";
 
 // Import your images here
-import img1 from "../assets/img1.jpg";
-import img2 from "../assets/img2.jpg";
-import img3 from "../assets/img3.jpg";
-import img4 from "../assets/img4.jpg";
-import img5 from "../assets/img5.jpg";
-import img6 from "../assets/img6.jpg";
-import img7 from "../assets/img7.jpg";
-import img8 from "../assets/img8.jpg";
-import img9 from "../assets/img9.jpg";
-import img10 from "../assets/img10.jpg";
-import img11 from "../assets/img11.jpg";
-import img12 from "../assets/img12.jpg";
+import img1 from "../assets/church1.jpg";
+//import img2 from "../assets/img2.jpg";
+//import img3 from "../assets/img3.jpg";
+//import img4 from "../assets/img4.jpg";
+//import img5 from "../assets/img5.jpg";
+//import img6 from "../assets/img6.jpg";
+//import img7 from "../assets/img7.jpg";
+//import img8 from "../assets/img8.jpg";
+//import img9 from "../assets/img9.jpg";
+//import img10 from "../assets/img10.jpg";
+//import img11 from "../assets/img11.jpg";
+//import img12 from "../assets/img12.jpg";
 
 const images = [
   { src: img1, name: "Image 1 description" , posting: "Image1 Posting", year: "year"},
-  { src: img2, name: "Image 1 description" , posting: "Image1 Posting", year: "year"},
-  { src: img3, name: "Image 1 description" , posting: "Image1 Posting", year: "year"},
-  { src: img4, name: "Image 1 description" , posting: "Image1 Posting", year: "year"},
-  { src: img5, name: "Image 1 description" , posting: "Image1 Posting", year: "year"},
-  { src: img6, name: "Image 1 description" , posting: "Image1 Posting", year: "year"},
-  { src: img7, name: "Image 1 description" , posting: "Image1 Posting", year: "year"},
-  { src: img8, name: "Image 1 description" , posting: "Image1 Posting", year: "year"},
-  { src: img9, name: "Image 1 description" , posting: "Image1 Posting", year: "year"},
-  { src: img10, name: "Image 1 description" , posting: "Image1 Posting", year: "year"},
-  { src: img11, name: "Image 1 description" , posting: "Image1 Posting", year: "year"},
-  { src: img12, name: "Image 1 description" , posting: "Image1 Posting", year: "year"},
+  
 ];
 
 const ImageGrid = () => {
