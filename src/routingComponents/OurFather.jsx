d0c36cba9e132b8811e21559839bacd81d2b4bb1
ak0kombat <ak0kombat@gmail.com>
2025-10-25 14:41:52 +0530

    Update OurFather.jsx

--- src/routingComponents/OurFather.jsx
+++ src/routingComponents/OurFather.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 
 // Import your images here
-import img1 from "../assets/church1.jpg";
-//import img2 from "../assets/img2.jpg";
-//import img3 from "../assets/img3.jpg";
-//import img4 from "../assets/img4.jpg";
-//import img5 from "../assets/img5.jpg";
-//import img6 from "../assets/img6.jpg";
-//import img7 from "../assets/img7.jpg";
-//import img8 from "../assets/img8.jpg";
-//import img9 from "../assets/img9.jpg";
-//import img10 from "../assets/img10.jpg";
-//import img11 from "../assets/img11.jpg";
-//import img12 from "../assets/img12.jpg";
+import img1 from "src/assets/fatherImg/cfather1.jpg";
+import img2 from "src/assets/fatherImg/cfather2.jpg";
+import img3 from "src/assets/fatherImg/cfather3.jpg";
+import img4 from "src/assets/fatherImg/cfather4.jpg";
+import img5 from "src/assets/fatherImg/cfather5.jpg";
+import img6 from "src/assets/fatherImg/cfather6.jpg";
+import img7 from "src/assets/fatherImg/cfather7.jpg";
+import img8 from "src/assets/fatherImg/cfather8.jpg";
+import img9 from "src/assets/fatherImg/cfather9.jpg";
+import img10 from "src/assets/fatherImg/cfather10.jpg";
+import img11 from "src/assets/fatherImg/cfather11.jpg";
+import img12 from "src/assets/fatherImg/father3.jpg";
 
 const images = [
-  { src: img1, name: "Image 1 description" , posting: "Image1 Posting", year: "year"},
+  { src: img1, name: "Rev.R.V.Mathias" , posting: "First Parish Priest", year: "01-05-1967 to 01-05-1972"},
+  { src: img2, name: "Rev.A.Soosairaj" , posting: "Second Parish Priest", year: "01-05-1972 to 14-06-1981"},
+  { src: img3, name: "Rev.M.S.Lawrence" , posting: "Third Parish Priest", year: "14-08-1981 to 26-05-1980"},
+  { src: img4, name: "Rev.P.Thomas Balsamy" , posting: "Fourth Parish Priest", year: "26-05-1985 to 28-09-1986"},
+  { src: img5, name: "Rev.V.M.Marinayakam" , posting: "Fifth Parish Priest", year: "28-09-1986 to 14-06-1987"},
+  { src: img6, name: "Rev.A.Arputham" , posting: "Sixth Parish Priest", year: "14-06-1987 to 04-06-1994"},
+  { src: img7, name: "Rev.A.Vincent" , posting: "Seventh Parish Priest", year: "04-06-1994 to 17-06-2001"},
+  { src: img8, name: "Rev.A.Soosairaj" , posting: "Eighth Parish Priest", year: "17-06-2001 to 02-06-2006"},
+  { src: img9, name: "Rev.A.James" , posting: "Nineth Parish Prince", year: "02-06-2006 to 05-06-2011"},
+  { src: img10, name: "Rev.A.Sundhararaj" , posting: "Tenth Parish Priest", year: "2011 to 2016"},
+  { src: img11, name: "Rev.M.Gnanadhikam" , posting: "Eleventh Parish Priest", year: "2016 to 2024"},
+  { src: img12, name: "Fr. Jerome gnana prabu" , posting: "Twelveth Parish Priest", year: "2024 to 2028"}
   
 ];
 
@@ -37,6 +48,12 @@ const ImageGrid = () => {
             {/* Description below image */}
             <p className="mt-4 text-center text-gray-800 text-sm font-medium">
               {image.name}
+            </p>
+            <p className="mt-4 text-center text-gray-800 text-sm font-medium">
+              {image.posting}
+            </p>
+            <p className="mt-4 text-center text-gray-800 text-sm font-medium">
+              Time Period: {image.year}
             </p>
           </div>
         ))}
